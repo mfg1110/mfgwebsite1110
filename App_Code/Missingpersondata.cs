d1d74372d1cd97cbef2549ce0f5fb2961adc4919
mfg1110 <92136237+mfg1110@users.noreply.github.com>
2021-10-12 08:32:16 +0530

    add appcode

---
+++ App_Code/Missingpersondata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Missingpersondata
+/// </summary>
+public class Missingpersondata
+{
+	public Missingpersondata()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
